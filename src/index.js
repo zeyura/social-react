86dafd2439d491270d14af49477e426c53d9dfa3
zeyura <zeyura75@gmail.com>
2020-12-28 21:41:33 +0200

    тут точно: убрал подписчик и ререндер из индекса

--- src/index.js
+++ src/index.js
@@ -7,21 +7,14 @@ import App from './App'
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>, document.getElementById('root')
-    );
-}
 
-rerenderEntireTree();
-
-store.subscribe(() => {
-    rerenderEntireTree();
-});
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>, document.getElementById('root')
+);    
 
 
 // API
